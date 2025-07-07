8b532e85a18442939a8e4cd904aefe736fadf08e
akash-aman <sir.akashaman@gmail.com>
2025-07-07 09:04:09 +0530

    refactor: simplify exports in index.ts for cleaner code structure

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,23 +1,2 @@
-export {
-    Bond,
-    Node,
-    Layout,
-    Queue,
-    createReactiveState,
-    areNodeOptionsMapEqual,
-    areNodeOptionsEqual,
-} from './app/index';
-
-export type {
-    BaseNode,
-    ChangeListener,
-    NodeOptions,
-    NodeType,
-    Dimension,
-    LayoutTree,
-    NodeCache,
-    NodeTypeWithBond,
-    ReactiveValue,
-    RootAdjustment
-
-} from './type';
+export * from './app/index';
+export type * from './type';
